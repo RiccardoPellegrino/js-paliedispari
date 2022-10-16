d4c64f46059cabd41eb070d688888f4c831476cb
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-16 17:58:38 +0200

    Pari e dispari + modifiche

--- js/libreria.js
+++ js/libreria.js
@@ -6,3 +6,45 @@ function isPalindroma(str) {
     
     return reverseStr === lowStr;
 }
+
+function getRandomNumber(min, max){
+
+    return  Math.floor(Math.random() * (max - min +1) + min );
+  
+  }
+
+function pariDispari(num1,num2) {
+    
+    let sum = num1 + num2;
+
+    console.log(sum);
+
+    if(sum % 2){
+        verifica = false;
+        scelta = "dispari";
+    }else{
+        verifica = true; 
+        scelta = "pari";
+       }
+
+    return sum;
+
+  }
+
+  function controllaInput(numeroParsato){
+
+    if (isNaN(numeroParsato)) {
+        document.getElementById('messaggio2').innerHTML = "";
+        document.getElementById('messaggio3').innerHTML = "";
+        
+        alert('Devi inserire un numero');
+        
+    }
+    else if (numeroParsato > 5 || numeroParsato < 1) {
+        alert("Devi inserire un numero da 1 a 5 !")
+    }
+
+    else{
+        return true;
+    }
+  }
--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ let parolaUtente = document.getElementById('testo');
 let btn = document.getElementById("bottone");
 
 const check = function () {
-
+  
     if (parolaUtente.value == '') {
-
-        alert('non hai inserito nulla')
+        document.getElementById('messaggio').innerHTML = '';
+        alert('Non hai inserito una parola')
     }
     else if (isPalindroma(parolaUtente.value)) {
 
@@ -15,10 +15,78 @@ const check = function () {
     } else {
 
         document.getElementById('messaggio').innerHTML = 'La tua parola non è Palindroma';
-       
+
     }
-    
-    document.getElementById("testo").value ='';
+
+    document.getElementById("testo").value = '';
 }
 
 btn.addEventListener("click", check);
+
+
+
+
+
+
+let numeroUtente = document.getElementById("numero");
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+
+const check1 = function () {
+
+    const numeroParsato = parseInt(numeroUtente.value);
+
+    if (controllaInput(numeroParsato)){
+
+        let numeroRandom = getRandomNumber(1, 5);
+        let numeroFinale = (numeroParsato + numeroRandom);
+
+        document.getElementById('messaggio3').innerHTML = "Il computer ha generato casualmente "+ numeroRandom + " sommato al tuo "+numeroParsato +" si ottiene " + numeroFinale;
+
+        if(numeroFinale % 2 == 0){
+            document.getElementById('messaggio2').innerHTML = "Hai vinto";
+        }
+        else{
+            document.getElementById('messaggio2').innerHTML = "Hai Perso";
+        }
+
+       
+    }
+    document.getElementById("numero").value = '';
+}
+
+btn1.addEventListener("click", check1);
+
+
+const check2 = function () {
+
+    const numeroParsato = parseInt(numeroUtente.value);
+
+    if (controllaInput(numeroParsato)){
+
+        let numeroRandom = getRandomNumber(1, 5);
+        let numeroFinale = (numeroParsato + numeroRandom);
+
+        document.getElementById('messaggio3').innerHTML = "Il computer ha generato casualmente "+ numeroRandom + " sommato al tuo "+numeroParsato +" si ottiene " + numeroFinale;
+
+        if(numeroFinale % 2 != 0){
+            document.getElementById('messaggio2').innerHTML = "Hai vinto";
+        }
+        else{
+            document.getElementById('messaggio2').innerHTML = "Hai Perso";
+        }
+
+       
+    }
+    document.getElementById("numero").value = '';
+}
+
+btn2.addEventListener("click", check2);
+
+
+let btnReset = document.getElementById("btn-reset");
+
+btnReset.addEventListener("click", function(){
+    window.location.reload();
+});
